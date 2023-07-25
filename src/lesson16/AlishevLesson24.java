e23ec3e90ff9e1bcf37753a46682df6b83f7a827
Valerii1129 <yanovskiyvalera1@gmail.com>
2023-07-25 22:48:27 +0100

    lesson16 1commit

---
+++ src/lesson16/AlishevLesson24.java
@@ -0,0 +1,8 @@
+package lesson16;
+
+public class AlishevLesson24 {
+    public static void main(String[] args) {
+        System.out.printf("This is a string, %s", "NICE");
+        System.out.printf("String %.2f",45.23412312);
+    }
+}
---
+++ src/lesson16/AlishevLesson25.java
@@ -0,0 +1,24 @@
+package lesson16;
+
+public class AlishevLesson25 {
+    public static void main(String[] args) {
+        People p1 = new People("Bob",40);
+        System.out.println(p1);
+
+    }
+}
+
+class People{
+    private String name;
+    private int age;
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name+", "+age;
+    }
+}
