e383778ff35d372a5f6d2388c0f8133802e31d2d
Valerii1129 <yanovskiyvalera1@gmail.com>
2023-08-04 10:59:41 +0100

    lesson11 4commit

--- src/lesson11/Start.java
+++ src/lesson11/Start.java
@@ -3,10 +3,10 @@ package hillel.json1.service;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-import hillel.json1.model.animal.Animal;
 
 public class Start {
     public static void main(String[] args) {
+        final Animal dog = new Animal("sharik", "2009.02.12", 14, false, "buldog");
 
         final AnimalForAllTypeSerializer<JsonMapper> animalSerializer1 = new AnimalForAllTypeSerializer<>(new JsonMapper());
         animalSerializer1.serialize(dog);
